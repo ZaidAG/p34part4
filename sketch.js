11218606f6ab24f3004b60ca76f4ba60f6372564
ZaidAG <72582743+ZaidAG@users.noreply.github.com>
2021-03-03 09:13:01 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock;
+function preload()
+{
+	//load images here
+  dog.loadImage()
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
